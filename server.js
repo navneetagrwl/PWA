d527e4bd92e6afbc74387d556b9c5e2d4fd8ff43
navneetagrwl <34672954+navneetagrwl@users.noreply.github.com>
2018-12-17 21:22:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.post('/prices/new', (req, res) => {
     res.sendStatus(200);
 })
 
-app.listen(process.env.PORT || app.get('port'), () => {
-    console.log('Node app is running on port', app.get('port'))
+app.listen(process.env.PORT || 5000, () => {
+//     console.log('Node app is running on port', app.get('port'))
 })
